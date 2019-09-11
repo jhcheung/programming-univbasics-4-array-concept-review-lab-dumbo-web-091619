b2200398555ee1f040fa55237d27eee737abe9d9
artificial-semicolon-2761 <jimmy.h.cheung@gmail.com>
2019-09-11 21:37:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,8 +8,12 @@ end
 
 def find_max_value(array)
   max = 0
-  array.length.times do 
-    if 
+  array.length.times do |index|
+    if array[index] > max
+      max = array[index]
+    end
+  end
+  return max
 end
 
 def find_min_value(array)
