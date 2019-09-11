25b0d7aea21f9ffc1c96738225303e1b24540227
artificial-semicolon-2761 <jimmy.h.cheung@gmail.com>
2019-09-11 21:27:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
-  counter = array.length
-  counter.times do |index|
-    if array[index] == value_to_find 
-      return index
-  end
+  counter = 0 
+  while counter < array.length do
+    if array[counter] == value_to_find return counter
+    counter += 1
+  end 
 end
 
 def find_max_value(array)
