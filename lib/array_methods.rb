ee8083871f9638325273cad631f6c51b387c5db6
artificial-semicolon-2761 <jimmy.h.cheung@gmail.com>
2019-09-11 21:34:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,5 @@
 def find_element_index(array, value_to_find)
-  (array.length).times do |index|
+  array.length.times do |index|
     if array[index] == value_to_find 
       return index
     end 
@@ -7,7 +7,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  
 end
 
 def find_min_value(array)
