719c98772de6f6af55b8824687151686c1c556a8
artificial-semicolon-2761 <jimmy.h.cheung@gmail.com>
2019-09-11 21:33:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,20 @@
 def find_element_index(array, value_to_find)
   (array.length).times do |index|
-    if array[index] == value_to_find return index
+    if array[index] == value_to_find 
+      return index
     end 
   end
   return nil
 end
+
+def find_element_index(array, value_to_find)
+ (array.length).times do |v|
+  if array[v] == value_to_find
+    return v 
+  end
+end 
+return nil 
+end 
 
 def find_max_value(array)
   # Add your solution here
